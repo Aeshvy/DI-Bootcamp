af5234e0de4552d44ffffe09981770e04b3d6b5b
Alex <aeshvy@gmail.com>
2025-03-24 19:04:44 +0200

    Python - week2

--- week2/day2/ExerciseXP/ExerciseXP.py
+++ week2/day2/ExerciseXP/ExerciseXP.py
@@ -122,6 +122,12 @@ make_great()
 # 🌟 Exercise 7 : Temperature Advice
 
 # Create a function called get_random_temp().
+def get_random_temp():
+    return random.randint(-10, 40)
+
+print(get_random_temp())
+
+
 # This function should return an integer between -10 and 40 degrees (Celsius), selected at random.
 # Test your function to make sure it generates expected results.
 
