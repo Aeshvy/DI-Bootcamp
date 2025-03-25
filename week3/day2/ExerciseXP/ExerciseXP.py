c0ed3ff3ef66680a0c30932e92e4987b28ff3c48
Alex <aeshvy@gmail.com>
2025-03-25 22:31:26 +0200

    Python - week3

--- week3/day2/ExerciseXP/ExerciseXP.py
+++ week3/day2/ExerciseXP/ExerciseXP.py
@@ -1,6 +1,5 @@
 # 🌟 Exercise 1 : Pets
 
-
 class Pets():
     def __init__(self, animals):
         self.animals = animals
@@ -31,12 +30,90 @@ class Siamese(Cat):
     def sing(self, sounds):
         return f'{sounds}'
 
-all_cats = [Bengal, Chartreux, Siamese]
+# all_cats = [Bengal, Chartreux, Siamese]
 
-sara_pets = Pets(all_cats) 
+bengal = Bengal("Rob", 3)
+chartreux = Chartreux("Bob", 4)
+siamese = Siamese("Knob", 5)
+
+all_cats = [bengal, chartreux, siamese]
+
+sara_pets = Pets(all_cats)
+
+sara_pets.walk()
+
 
     
 # Create another cat breed named Siamese which inherits from the Cat class.
 # Create a list called all_cats, which holds three cat instances : one Bengal, one Chartreux and one Siamese.
 # Those three cats are Sara’s pets. Create a variable called sara_pets which value is an instance of the Pet class, and pass the variable all_cats to the new instance.
 # Take all the cats for a walk, use the walk method.
+
+# ---------------------------------------------------------------------------
+
+# 🌟 Exercise 2 : Dogs
+
+# Create a class called Dog with the following attributes name, age, weight.
+# Implement the following methods in the Dog class:
+# bark: returns a string which states: “<dog_name> is barking”.
+# run_speed: returns the dogs running speed (weight/age*10).
+# fight : takes a parameter which value is another Dog instance, called other_dog. This method returns a string stating which dog won the fight. The winner should be the dog with the higher run_speed x weight.
+
+# Create 3 dogs and run them through your class.
+
+class Dog():
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    def bark(self):
+        return f'{self.name} is barking'
+
+    def run_speed(self):
+        return (self.weight / self.age) * 10
+
+    def fight(self, other_dog):
+        self_score = self.run_speed() * self.weight
+        other_dog_score = other_dog.run_speed() * other_dog.weight
+
+        if self_score > other_dog_score:
+            return f"{self.name} wins the fight!"
+        elif self_score < other_dog_score:
+            return f"{other_dog.name} wins the fight!"
+
+dog1 = Dog("Max", 3, 20)
+dog2 = Dog("Lax", 2, 25)
+dog3 = Dog("Dax", 4, 18)
+
+# print(dog1.bark())
+# print(dog2.bark())
+# print(dog3.bark())
+
+# print(f"Max's run speed is {dog1.run_speed():.2f}")
+# print(f"Lax's run speed is {dog2.run_speed():.2f}")
+# print(f"Dax's run speed is {dog3.run_speed():.2f}")
+
+# print(dog1.fight(dog2))
+# print(dog1.fight(dog3))
+# print(dog3.fight(dog2))
+
+# ---------------------------------------------------------------------------
+
+# 🌟 Exercise 3 : Dogs Domesticated
+
+# Create a new python file and import your Dog class from the previous exercise.
+# In the new python file, create a class named PetDog that inherits from Dog.
+# Add an attribute called trained to the __init__ method, this attribute is a boolean and the value should be False by default.
+# Add the following methods:
+# train: prints the output of bark and switches the trained boolean to True
+
+# play: takes a parameter which value is a few names of other Dog instances (use *args). The method should print the following string: “dog_names all play together”.
+
+# do_a_trick: If the dog is trained the method should print one of the following sentences at random:
+# “dog_name does a barrel roll”.
+# “dog_name stands on his back legs”.
+# “dog_name shakes your hand”.
+# “dog_name plays dead”.
+
+
---
+++ week3/day2/ExerciseXP/Import.py
@@ -0,0 +1,59 @@
+from ExerciseXP import Dog
+import random
+
+# 🌟 Exercise 3 : Dogs Domesticated
+
+# Create a new python file and import your Dog class from the previous exercise.
+# In the new python file, create a class named PetDog that inherits from Dog.
+# Add an attribute called trained to the __init__ method, this attribute is a boolean and the value should be False by default.
+
+# Add the following methods:
+
+# train: prints the output of bark and switches the trained boolean to True
+
+# play: takes a parameter which value is a few names of other Dog instances (use *args). The method should print the following string: “dog_names all play together”.
+
+# do_a_trick: If the dog is trained the method should print one of the following sentences at random:
+
+# “dog_name does a barrel roll”.
+# “dog_name stands on his back legs”.
+# “dog_name shakes your hand”.
+# “dog_name plays dead”.
+
+class PetDog(Dog):
+    def __init__(self, name, age, weight, trained = False):
+        super().__init__(name, age, weight)
+        self.trained = trained
+    
+    def train(self):
+        print(self.bark())
+        self.trained = True
+
+    def play(self, *args):
+        dog_names = ", ".join([self.name] + [dog.name for dog in args])
+        print(f'{dog_names} all play together.')
+
+    def do_a_trick(self):
+        if self.trained:
+            
+            tricks = [
+                f'{self.name} does a barrel roll.',
+                f'{self.name} stands on his back legs.',
+                f'{self.name} shakes your hand.',
+                f'{self.name} plays dead.'
+            ]
+            print(random.choice(tricks))
+        else:
+            print(f'{self.name} needs to be trained!')
+
+dog1 = PetDog("Bark", 3, 20)
+dog2 = PetDog("Dark", 2, 25)
+dog3 = PetDog("Lucy", 4, 18)
+
+dog1.train()
+
+dog1.do_a_trick()
+dog2.do_a_trick()
+dog3.do_a_trick()
+
+dog1.play(dog2, dog3)
