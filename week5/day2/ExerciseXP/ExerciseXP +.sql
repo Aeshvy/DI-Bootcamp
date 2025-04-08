d907491d71e495d66a7027e9c1d77136fe0fd5b9
Alex <aeshvy@gmail.com>
2025-04-08 14:36:27 +0300

    Python - week5

--- week5/day2/ExerciseXP/ExerciseXP +.sql
+++ week5/day2/ExerciseXP/ExerciseXP +.sql
@@ -34,12 +34,16 @@
 -- DELIMITER ','
 -- CSV HEADER;
 
--- SET DateStyle = 'ISO, DMY';
-
--- SELECT * FROM students
--- SELECT first_name, last_name FROM students
--- SELECT * FROM students WHERE id = 2
-
+-- SELECT * FROM students;
+-- SELECT first_name, last_name FROM students;
+-- SELECT * FROM students WHERE id=2;
+-- SELECT * FROM students WHERE last_name='Benichou' OR first_name='Marc';
+-- SELECT * FROM students WHERE first_name LIKE '%a%';
+-- SELECT * FROM students WHERE first_name ILIKE 'a%';
+-- SELECT * FROM students WHERE first_name ILIKE '%a';
+-- SELECT * FROM students WHERE SUBSTRING(first_name, LENGTH(first_name) - 1, 1) = 'a';
+-- SELECT * FROM students WHERE id=1 OR id=3;
+SELECT * FROM students WHERE birth_date >= '1/01/2000';
 
 
 
