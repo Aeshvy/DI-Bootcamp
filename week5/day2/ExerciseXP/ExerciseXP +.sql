9559e6cf2c6f1f015a05091408128963ed34c0c8
Alex <aeshvy@gmail.com>
2025-04-08 14:05:32 +0300

    Python - week5

--- week5/day2/ExerciseXP/ExerciseXP +.sql
+++ week5/day2/ExerciseXP/ExerciseXP +.sql
@@ -21,14 +21,18 @@
 -- birth_date DATE NOT NULL
 -- )
 
-INSERT INTO students(first_namme, last_name, birth_date)
-VALUES('Marc', 'Benichou', '02/11/1998')
-('Yoan', 'Cohen', '03/12/2010')
-('Lea', 'Benichou', '27/07/1987')
-('Amelia', 'Dux', '07/04/1996')
-('David', 'Grez', '14/06/2003')
-('Omer', 'Simpson', '03/10/1980')
+-- INSERT INTO students(first_namme, last_name, birth_date)
+-- VALUES('Marc', 'Benichou', '02/11/1998'),
+-- ('Yoan', 'Cohen', '03/12/2010'),
+-- ('Lea', 'Benichou', '27/07/1987'),
+-- ('Amelia', 'Dux', '07/04/1996'),
+-- ('David', 'Grez', '14/06/2003'),
+-- ('Omer', 'Simpson', '03/10/1980');
 
+COPY students
+FROM 'C:\Users\eshvo\Desktop\Fullstack\DI-Bootcamp\week5\day2\ExerciseXP\students.csv'
+DELIMITER ','
+CSV HEADER;
 
-SELECT * FROM students
+-- SELECT * FROM students
 
