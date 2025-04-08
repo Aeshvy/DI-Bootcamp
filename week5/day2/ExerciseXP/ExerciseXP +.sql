c2cd43dd4706cdd1a1da37b4d59f2e08667dfb83
Alex <aeshvy@gmail.com>
2025-04-08 14:56:04 +0300

    Python - week5

--- week5/day2/ExerciseXP/ExerciseXP +.sql
+++ week5/day2/ExerciseXP/ExerciseXP +.sql
@@ -14,36 +14,43 @@
 --     IS_TEMPLATE = False;
 
 ----------------------------------------------------------------------
-CREATE TABLE students(
-id SERIAL PRIMARY KEY,
-first_name VARCHAR (50) NOT NULL,
-last_name VARCHAR (100) NOT NULL,
-birth_date DATE NOT NULL
-)
+-- CREATE TABLE students(
+-- id SERIAL PRIMARY KEY,
+-- first_name VARCHAR (50) NOT NULL,
+-- last_name VARCHAR (100) NOT NULL,
+-- birth_date DATE NOT NULL
+-- )
 
-INSERT INTO students(first_name, last_name, birth_date)
-VALUES('Marc', 'Benichou', '11/02/1998'),
-('Yoan', 'Cohen', '12/03/2010'),
-('Lea', 'Benichou', '07/27/1987'),
-('Amelia', 'Dux', '04/07/1996'),
-('David', 'Grez', '06/14/2003'),
-('Omer', 'Simpson', '10/03/1980');
+-- INSERT INTO students(first_name, last_name, birth_date)
+-- VALUES('Marc', 'Benichou', '11/02/1998'),
+-- ('Yoan', 'Cohen', '12/03/2010'),
+-- ('Lea', 'Benichou', '07/27/1987'),
+-- ('Amelia', 'Dux', '04/07/1996'),
+-- ('David', 'Grez', '06/14/2003'),
+-- ('Omer', 'Simpson', '10/03/1980');
 
-COPY students(first_name, last_name, birth_date)
-FROM 'students.csv'
-DELIMITER ','
-CSV HEADER;
+-- COPY students(first_name, last_name, birth_date)
+-- FROM 'students.csv'
+-- DELIMITER ','
+-- CSV HEADER;
 
-SELECT * FROM students;
-SELECT first_name, last_name FROM students;
-SELECT * FROM students WHERE id=2;
-SELECT * FROM students WHERE last_name='Benichou' OR first_name='Marc';
-SELECT * FROM students WHERE first_name LIKE '%a%';
-SELECT * FROM students WHERE first_name ILIKE 'a%';
-SELECT * FROM students WHERE first_name ILIKE '%a';
-SELECT * FROM students WHERE SUBSTRING(first_name, LENGTH(first_name) - 1, 1) = 'a';
-SELECT * FROM students WHERE id=1 OR id=3;
-SELECT * FROM students WHERE birth_date >= '1/01/2000';
+-- SELECT * FROM students;
+-- SELECT first_name, last_name FROM students;
+-- SELECT * FROM students WHERE id=2;
+-- SELECT * FROM students WHERE last_name='Benichou' OR first_name='Marc';
+-- SELECT * FROM students WHERE first_name LIKE '%a%';
+-- SELECT * FROM students WHERE first_name ILIKE 'a%';
+-- SELECT * FROM students WHERE first_name ILIKE '%a';
+-- SELECT * FROM students WHERE SUBSTRING(first_name, LENGTH(first_name) - 1, 1) = 'a';
+-- SELECT * FROM students WHERE id=1 OR id=3;
+-- SELECT * FROM students WHERE birth_date >= '1/01/2000';
+----------------------------------------------------------------------
 
+-- Continuation of the Exercise XP +
+
+-- SELECT * FROM students ORDER BY last_name ASC LIMIT 4;
+-- SELECT * FROM students ORDER BY birth_date ASC LIMIT 1;
+-- SELECT * FROM students OFFSET 2 LIMIT 3;
+-- SELECT * FROM students;
 
 
