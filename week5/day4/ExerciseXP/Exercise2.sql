a40657de92449e9b8f6b0a02086fff2fd56ad84c
Alex <aeshvy@gmail.com>
2025-04-09 20:44:45 +0300

    Python - week5

--- week5/day4/ExerciseXP/Exercise2.sql
+++ week5/day4/ExerciseXP/Exercise2.sql
@@ -1,11 +1,13 @@
 🌟 Exercise 2 : dvdrental database
 
 -- In the dvdrental database write a query to select all the columns from the “customer” table.
-SELECT * FROM customer
+SELECT * FROM customer;
 
 -- Write a query to display the names (first_name, last_name) using an alias named “full_name”.
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM customer;
 
 -- Lets get all the dates that accounts were created. Write a query to select all the create_date from the “customer” table (there should be no duplicates).
+SELECT DISTINCT create_date FROM customer;
 
 -- Write a query to get all the customer details from the customer table, it should be displayed in descending order by their first name.
 
