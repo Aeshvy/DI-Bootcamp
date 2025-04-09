954317085f663f6437ade6e88f062b08e7df9714
Alex <aeshvy@gmail.com>
2025-04-09 22:26:48 +0300

    Python - week5

--- week5/day4/ExerciseXP/Exercise2.sql
+++ week5/day4/ExerciseXP/Exercise2.sql
@@ -10,23 +10,32 @@ SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM customer;
 SELECT DISTINCT create_date FROM customer;
 
 -- Write a query to get all the customer details from the customer table, it should be displayed in descending order by their first name.
+SELECT * FROM customer ORDER BY first_name DESC;
 
 -- Write a query to get the film ID, title, description, year of release and rental rate in ascending order according to their rental rate.
+SELECT film_id, title, description, release_year FROM film ORDER BY rental_rate;
 
 -- Write a query to get the address, and the phone number of all customers living in the Texas district, these details can be found in the “address” table.
+SELECT address, phone FROM address WHERE district='Texas';
 
 -- Write a query to retrieve all movie details where the movie id is either 15 or 150.
+SELECT * FROM film WHERE film_id=15 OR film_id=150;
 
 -- Write a query which should check if your favorite movie exists in the database. Have your query get the film ID, title, description, length and the rental rate, these details can be found in the “film” table.
+SELECT film_id, title, description, length, rental_rate FROM film WHERE title='Carol Texas';
 
 -- No luck finding your movie? Maybe you made a mistake spelling the name. Write a query to get the film ID, title, description, length and the rental rate of all the movies starting with the two first letters of your favorite movie.
+SELECT film_id, title, description, length, rental_rate FROM film WHERE title iLIKE 'ca%';
 
 -- Write a query which will find the 10 cheapest movies.
+SELECT rental_rate FROM film ORDER BY rental_rate ASC LIMIT 10;
 
 -- Not satisfied with the results. Write a query which will find the next 10 cheapest movies.
 -- Bonus: Try to not use LIMIT.
+SELECT rental_rate FROM film ORDER BY rental_rate ASC OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
 
 -- Write a query which will join the data in the customer table and the payment table. You want to get the first name and last name from the curstomer table, as well as the amount and the date of every payment made by a customer, ordered by their id (from 1 to…).
+
 
 -- You need to check your inventory. Write a query to get all the movies which are not in inventory.
 
