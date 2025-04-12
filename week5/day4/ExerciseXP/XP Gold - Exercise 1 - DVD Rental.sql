631d45e8ab8f206ae8afc8ad85febdfb9496ecb0
Alex <aeshvy@gmail.com>
2025-04-12 21:39:50 +0300

    Python - week5

--- week5/day4/ExerciseXP/XP Gold - Exercise 1 - DVD Rental.sql
+++ week5/day4/ExerciseXP/XP Gold - Exercise 1 - DVD Rental.sql
@@ -7,7 +7,17 @@ ORDER BY movie_count DESC;
 
 -- Find a customer in the customer table, and change his/her details to your details, using SQL UPDATE.
 -- Now find the customer’s address, and use UPDATE to change the address to your address (or make one up).
+UPDATE customer
+SET first_name = 'Alexander', last_name = 'Eshy', email = 'Eshvovych@gmail.com'
+WHERE customer_id = 1;
 
+UPDATE address
+SET address = '23 Jan Kur', district = 'Netanya'
+WHERE address_id = 5;
 
 -- Get a list of all the movies that have a rating of G or PG-13.
 -- Filter this list further: look for only movies that are under 2 hours long, and whose rental price (rental_rate) is under 3.00. Sort the list alphabetically.
+SELECT film_id, title, rating, rental_rate
+FROM film
+WHERE (rating = 'G' OR rating = 'PG-13') AND length < 120 AND rental_rate < 3.00
+ORDER BY title ASC;
