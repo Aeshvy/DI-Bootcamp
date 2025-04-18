6877ac5f15ec425b7d6dd0586efdc045029d4b3a
Alex <aeshvy@gmail.com>
2025-04-19 00:16:27 +0300

    Python - week5

---
+++ week5/day5/ExerciseXP/ExerciseXP - Exercise 1.sql
@@ -0,0 +1,70 @@
+🌟 Exercise 1: DVD Rental
+
+-- Get a list of all the languages, from the language table.
+SELECT name FROM language;
+
+-- Get a list of all films joined with their languages – select the following details : film title, description, and language name.
+SELECT title, description, language.name AS language_name
+FROM film
+LEFT JOIN language ON film.language_id = language.language_id;
+
+-- Get all languages, even if there are no films in those languages – select the following details : film title, description, and language name.
+SELECT title, description, language.name AS language_name
+FROM language
+LEFT JOIN film ON film.language_id = language.language_id;
+
+-- Create a new table called new_film with the following columns : id, name. Add some new films to the table.
+CREATE TABLE new_film (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100)
+);
+
+INSERT INTO new_film (name)
+VALUES 
+('The Lighthouse'),
+('Everything Everywhere All At Once'),
+('Inception'),
+('Parasite'),
+('Interstellar');
+
+-- Create a new table called customer_review, which will contain film reviews that customers will make.
+-- Think about the DELETE constraint: if a film is deleted, its review should be automatically deleted.
+-- It should have the following columns:
+-- review_id – a primary key, non null, auto-increment.
+-- film_id – references the new_film table. The film that is being reviewed.
+-- language_id – references the language table. What language the review is in.
+-- title – the title of the review.
+-- score – the rating of the review (1-10).
+-- review_text – the text of the review. No limit on the length.
+-- last_update – when the review was last updated.
+
+CREATE TABLE customer_review (
+review_id SERIAL NOT NULL PRIMARY KEY,
+film_id INT,
+language_id INT,
+title TEXT,
+score INT,
+review_text TEXT,
+last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+FOREIGN KEY (film_id) REFERENCES new_film(id) ON DELETE CASCADE,
+FOREIGN KEY (language_id) REFERENCES language (language_id)
+);
+
+-- Add 2 movie reviews. Make sure you link them to valid objects in the other tables.
+INSERT INTO customer_review (film_id, language_id, title, score, review_text, last_update)
+VALUES (5, 1, 'Outstanding!', 10, )
+
+INSERT INTO customer_review (customer_id, film_id, review_text, rating)
+VALUES
+    (1, 10, 'Absolutely loved it. Great storytelling!', 5),
+    (2, 12, 'Pretty good, but the pacing was a bit slow.', 4);
+
+
+
+Delete a film that has a review from the new_film table, what happens to the customer_review table?
+
+
+SELECT * FROM customer_review
+SELECT * FROM new_film
+
