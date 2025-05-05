161da6089a6c9b09fdf8983b54b1d0e4bd729cf9
Alex <aeshvy@gmail.com>
2025-05-05 21:24:32 +0300

    Python - week7

--- week7/day4/ExerciseXP/Exercise2.js
+++ week7/day4/ExerciseXP/Exercise2.js
@@ -3,14 +3,13 @@
 // Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
 // Hint : create an array of suffixes to do the Bonus
 
-const colors = ["Red", "Blue", "Teal", "Black", "Orange"]
-const suffixes = ["st", "nd", "rd", "th", "th"]
+const colors = ["Red", "Blue", "Teal", "Black", "Orange"];
+const suffixes = ["st", "nd", "rd", "th", "th"];
 
 for (let i = 0; i < colors.length; i++) {
-    console.log(`My #${i + 1} choice is ${colors[i]}`)
+  console.log(`My #${i + 1} choice is ${colors[i]}`);
 }
 
 for (let i = 0; i < colors.length; i++) {
-    console.log(`My ${i + 1}${suffixes[i]} choice is ${colors[i]}`)
+  console.log(`My ${i + 1}${suffixes[i]} choice is ${colors[i]}`);
 }
-
--- week7/day4/ExerciseXP/Exercise3.js
+++ week7/day4/ExerciseXP/Exercise3.js
@@ -4,17 +4,16 @@
 // While the number is smaller than 10 continue asking the user for a new number.
 // Tip : Which while loop is more relevant for this situation?
 
-let userInput = prompt("Input a number: ")
+let userInput = prompt("Input a number: ");
 if (isNaN(userInput)) {
-console.log("You entered a string!")
+  console.log("You entered a string!");
 } else {
-console.log("You entered an integer!")
+  console.log("You entered an integer!");
 }
 
 while (userInput < 10) {
-    userInput = prompt("Input a number: ")
-    console.log("The number is less than 10!")
-    userInput = Number(userInput)
+  userInput = prompt("Input a number: ");
+  console.log("The number is less than 10!");
+  userInput = Number(userInput);
 }
-console.log("The number is greater than 10!")
-
+console.log("The number is greater than 10!");
