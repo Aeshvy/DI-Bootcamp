35c98758d9753479d60cd5b99da3ccd95d1c3fc9
Alex <aeshvy@gmail.com>
2025-05-05 03:24:23 +0300

    Python - week7

---
+++ week7/day4/ExerciseXP/Exercise3.js
@@ -0,0 +1,20 @@
+// Prompt the user for a number.
+// Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
+
+// While the number is smaller than 10 continue asking the user for a new number.
+// Tip : Which while loop is more relevant for this situation?
+
+let userInput = prompt("Input a number: ")
+if (isNaN(userInput)) {
+console.log("You entered a string!")
+} else {
+console.log("You entered an integer!")
+}
+
+while (userInput < 10) {
+    userInput = prompt("Input a number: ")
+    console.log("The number is less than 10!")
+    userInput = Number(userInput)
+}
+console.log("The number is greater than 10!")
+
