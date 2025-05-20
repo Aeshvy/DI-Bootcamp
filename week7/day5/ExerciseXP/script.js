fc06cef77dd4b944b016f8eec70cf546bc4eb55c
Alex <aeshvy@gmail.com>
2025-05-20 16:12:39 +0300

    Python - week7

--- week7/day5/ExerciseXP/script.js
+++ week7/day5/ExerciseXP/script.js
@@ -29,36 +29,10 @@ for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i];
 }
 
-console.log("Sum:", sum);
+console.log("Total sum of numbers:", sum);
 
 
 // Exercise 2 : Shopping List
-
-const stock = { 
-    "banana": 6, 
-    "apple": 0,
-    "pear": 12,
-    "orange": 32,
-    "blueberry":1
-}  
-
-const prices = {    
-    "banana": 4, 
-    "apple": 2, 
-    "pear": 1,
-    "orange": 1.5,
-    "blueberry":10
-}
-
-let shoppingList = {
-    "banana": 1,
-    "orange": 1,
-    "apple": 1
-}
-
-function myBill() {
-    
-}
 
 // Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
 
@@ -69,3 +43,53 @@ function myBill() {
 // If the item is in stock find out the price in the prices object.
 
 // Call the myBill() function.
+
+// Bonus: If the item is in stock, decrease the item’s stock by 1
+
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}
+
+console.log(stock);
+currentStock = {...stock};
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+}
+
+const shoppingList = {
+    "banana": 3,
+    "orange": 3,
+    "apple": 3
+}
+
+function myBill() {
+    let total = 0;
+
+    for (let item in shoppingList) {
+        let quantity = shoppingList[item];
+
+        if (item in stock && stock[item] >= quantity && quantity > 0) {
+            total += prices[item] * quantity;
+            currentStock[item] -= quantity;
+        }
+    }
+    return total;
+}
+
+console.log(`The total bill is: $${myBill()}`);
+console.log(currentStock);
+
+
+// Exercise 3 : What’s in my wallet ?
+
+
+
