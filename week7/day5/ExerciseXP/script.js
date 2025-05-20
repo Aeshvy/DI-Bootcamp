d8b0c54112b7111e876fba2f9e3863a9ca9968f0
Alex <aeshvy@gmail.com>
2025-05-20 15:18:28 +0300

    Python - week7

--- week7/day5/ExerciseXP/script.js
+++ week7/day5/ExerciseXP/script.js
@@ -10,6 +10,9 @@
 
 function displayNumbersDivisible() {
   const divisibleNumbers = [];
+
+  console.log("All numbers divisible by 23:");
+
   for (let i = 0; i <= 500; i++) {
     if (i % 23 === 0) {
       console.log(i);
@@ -28,3 +31,41 @@ for (let i = 0; i < numbers.length; i++) {
 
 console.log("Sum:", sum);
 
+
+// Exercise 2 : Shopping List
+
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+}
+
+let shoppingList = {
+    "banana": 1,
+    "orange": 1,
+    "apple": 1
+}
+
+function myBill() {
+    
+}
+
+// Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
+
+// Create a function called myBill() that takes no parameters.
+
+// The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+// The item must be in stock. (Hint : check out if .. in)
+// If the item is in stock find out the price in the prices object.
+
+// Call the myBill() function.
